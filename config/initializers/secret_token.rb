d81f1942e01a24bd9940f18190e1133f7bba28b9
juankferro <juan@mediabyte.com.pe>
2014-04-19 12:44:16 -0500

    Creando aplicacion rails

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Holaheroku::Application.config.secret_key_base = '3f290c0db1947f4451b1c5cd1068afa9c19d51bb6276b725a823ba29e141abda04206806f49a7c2572bb692519deef73bfcbb14efc94a3afb470a6a66a1cb594'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+Holaheroku::Application.config.session_store :cookie_store, key: '_holaheroku_session'
